db8f43bd15d732719d4dfe25e3f5153f597e6c24
Mishael Bonel <mbonelortiz@gmail.com>
2024-10-24 10:13:40 +0200

    Agregar Relación1 Unidad4 Cliente

---
+++ DAW 2º/Aplicaciones web en cliente/Unidad 4. Interacción con el usuario/Ejercicios/relacion1.js
@@ -0,0 +1,12 @@
+// 1
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", ()=>alert("Has hecho click en el botón"));
+
+// 2
+const body = document.querySelector("body");
+
+body.addEventListener("mousemove", evento =>console.log("X: " + evento.clientX + ", Y: " + evento.clientY));
+
+// 4
+
