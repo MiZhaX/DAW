3a9ceabce6baf722efda761912aa797da45b7641
Mishael Bonel <mbonelortiz@gmail.com>
2024-11-08 12:31:48 +0100

    Añadir Relacion 2 Tema 3 Cliente

---
+++ DAW 2º/Aplicaciones web en cliente/Unidad 3. Objetos definidos por el Usuario/Ejercicios/relacion2.js
@@ -0,0 +1,33 @@
+// Ejercicio 5
+function paresImpares() {
+    const numeros = [];
+    for (let i = 0; i < 100; i++) {
+      numeros.push(Math.floor(Math.random() * 1000) + 1);
+    }
+  
+    console.log("Array inicial:", numeros);
+  
+    const pares = numeros.filter(num => num % 2 === 0);
+    const impares = numeros.filter(num => num % 2 !== 0);
+  
+    const arrayFinal = [pares, impares];
+  
+    console.log("Array final:", arrayOrganizado);
+}
+
+// Ejercicio 7
+function estableceACero(array) {
+    for (let i = 0; i < array.length; i++) {
+      array[i] = 0;
+    }
+}
+
+function sumarUno(array) {
+    for (let i = 0; i < array.length; i++) {
+      array[i] += 1;
+    }
+}
+
+function arrayConEspacios(array) {
+    console.log(array.join(' '));
+}
