f51b7d322cd6d1cd0558b8876e6138051ac0c527
Mishael Bonel <mbonelortiz@gmail.com>
2024-11-22 12:48:25 +0100

    Arreglar Colisiones, Añadir Inicio, Añadir Celebración

--- DAW 2º/Aplicaciones web en cliente/Unidad 5. DOM/Ejercicios/Pong/funciones.js
+++ DAW 2º/Aplicaciones web en cliente/Unidad 5. DOM/Ejercicios/Pong/funciones.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     // VARIABLES OBTENIDAS DEL DOCUMENTO
+    const botonEmpezar = document.getElementById("empezar");
     const pelota = document.getElementById("pelota");
     const jugador = document.getElementById("jugador");
     const marco = document.getElementById("pantalla");
@@ -32,92 +33,112 @@ window.onload = () => {
     var puntuacionJugador = 0;
     var puntuacionCPU = 0;
 
-    // AÑADIR EVENTO PARA MOVER AL JUGADOR CON W Y S
-    document.addEventListener("keydown", (event) => {
-        if (event.code == "KeyW" && jugadorY > 0) {
-            jugadorY -= 10;
-            jugador.setAttribute("y", jugadorY);
-        } else if (event.code == "KeyS" && (jugadorY + jugadorAltura) < limiteY) {
-            jugadorY += 10;
-            jugador.setAttribute("y", jugadorY);
-        }
+    // FUNCION QUE INICIA EL JUEGO AL HACER CLIC EN EL BOTÓN
+    botonEmpezar.addEventListener("click", () => {
+        botonEmpezar.style.display = "none"; 
+        iniciarJuego(); 
     });
 
-    // INTERVALO PARA MOVER LA PELOTA Y COMPROBAR LAS COLISIONES
-    setInterval(() => {
-        posicionX += velocidadX;
-        posicionY += velocidadY;
-
-        pelota.setAttribute("cx", posicionX);
-        pelota.setAttribute("cy", posicionY);
-
-        // OBTENER LOS PUNTOS CARDINALES DE LA PELOTA Y AGREGARLOS A UN ARRAY
-        var puntoA = posicionY - radio; // Coordenada Norte
-        var puntoB = Math.sqrt(Math.pow(posicionX + radio, 2) + Math.pow(posicionY - radio, 2)); // Coordenada Noreste
-        var puntoC = posicionX + radio; // Coordenada Este
-        var puntoD = Math.sqrt(Math.pow(posicionX + radio, 2) + Math.pow(posicionY + radio, 2)); // Coordenada Sureste
-        var puntoE = posicionY + radio; // Coordenada Sur
-        var puntoF = Math.sqrt(Math.pow(posicionX - radio, 2) + Math.pow(posicionY + radio, 2)); // Coordenada Suroeste
-        var puntoG = posicionX - radio; // Coordenada Oeste
-        var puntoH = Math.sqrt(Math.pow(posicionX - radio, 2) + Math.pow(posicionY - radio, 2)); // Coordenada Noroeste
-        var colisionPelota = [puntoA, puntoB, puntoC, puntoD, puntoE, puntoF, puntoG, puntoH];
-
-        // COLISIÓN CON LAS PAREDES
-        if ((posicionY + radio) > limiteY || (posicionY - radio) < 0) {
-            velocidadY *= -1;
-        }
-
-        // COLISIÓN CON EL JUGADOR
-        for (let i = 0; i < colisionPelota.length; i++) {
-            let punto = colisionPelota[i];
-            if (punto >= jugadorY && punto <= jugadorY + jugadorAltura && (posicionX - radio) <= jugadorX + jugadorAncho && (posicionX + radio) >= jugadorX) {
-                velocidadX *= -1;
-                velocidadX+=2;
-                velocidadY+=2;
-                break;
+    // FUNCION PARA INICIAR EL JUEGO
+    function iniciarJuego() {
+        // AÑADIR EVENTO PARA MOVER AL JUGADOR CON W Y S
+        document.addEventListener("keydown", (event) => {
+            if (event.code == "KeyW" && jugadorY > 0) {
+                jugadorY -= 10;
+                jugador.setAttribute("y", jugadorY);
+            } else if (event.code == "KeyS" && (jugadorY + jugadorAltura) < limiteY) {
+                jugadorY += 10;
+                jugador.setAttribute("y", jugadorY);
             }
-        }
+        });
 
-        // COLISIÓN CON LA CPU
-        for (let i = 0; i < colisionPelota.length; i++) {
-            let punto = colisionPelota[i];
-            if (punto >= cpuY && punto <= cpuY + cpuAltura && (posicionX - radio) <= cpuX + cpuAncho && (posicionX + radio) >= cpuX) {
-                velocidadX+=2;
-                velocidadY+=2;
-                velocidadX *= -1;
-                break;
+        // INTERVALO PARA MOVER LA PELOTA Y COMPROBAR LAS COLISIONES
+        setInterval(() => {
+            posicionX += velocidadX;
+            posicionY += velocidadY;
+
+            pelota.setAttribute("cx", posicionX);
+            pelota.setAttribute("cy", posicionY);
+
+            // OBTENER LOS PUNTOS CARDINALES DE LA PELOTA Y AGREGARLOS A UN ARRAY
+            var puntoA = posicionY - radio; // Coordenada Norte
+            var puntoB = Math.sqrt(Math.pow(posicionX + radio, 2) + Math.pow(posicionY - radio, 2)); // Coordenada Noreste
+            var puntoC = posicionX + radio; // Coordenada Este
+            var puntoD = Math.sqrt(Math.pow(posicionX + radio, 2) + Math.pow(posicionY + radio, 2)); // Coordenada Sureste
+            var puntoE = posicionY + radio; // Coordenada Sur
+            var puntoF = Math.sqrt(Math.pow(posicionX - radio, 2) + Math.pow(posicionY + radio, 2)); // Coordenada Suroeste
+            var puntoG = posicionX - radio; // Coordenada Oeste
+            var puntoH = Math.sqrt(Math.pow(posicionX - radio, 2) + Math.pow(posicionY - radio, 2)); // Coordenada Noroeste
+            var colisionPelota = [puntoA, puntoB, puntoC, puntoD, puntoE, puntoF, puntoG, puntoH];
+
+            // COLISIÓN CON LAS PAREDES
+            if ((posicionY + radio) > limiteY || (posicionY - radio) < 0) {
+                velocidadY *= -1;
             }
-        }
 
-        // MOVER LA CPU AUTOMÁTICAMENTE
-        if (posicionY > cpuY + cpuAltura / 2 && (cpuY + cpuAltura) < limiteY) {
-            cpuY += 5; 
-        } else if (posicionY < cpuY + cpuAltura / 2 && cpuY > 0) {
-            cpuY -= 5; 
-        }
-        cpu.setAttribute("y", cpuY);
+            // COLISIÓN CON EL JUGADOR
+            for (let i = 0; i < colisionPelota.length; i++) {
+                let punto = colisionPelota[i];
+                if (posicionX + radio > jugadorX && posicionX - radio < jugadorX + jugadorAncho && 
+                    posicionY + radio > jugadorY && posicionY - radio < jugadorY + jugadorAltura) {
+                    velocidadX *= -1;
+                    const offset = (posicionY - (jugadorY + jugadorAltura / 2)); 
+                    velocidadY = offset * 0.2;
+                    velocidadX += 2;
+                    velocidadY += 2;
+                    break;
+                }
+            }
 
-        // COMPROBAR SI ALGÚN JUGADOR OBTUVO UN PUNTO Y REINICIAR LA PELOTA EN CASO AFIRMATIVO
-        const punto = compruebaPunto(posicionX, limiteX, radio);
-        if (punto > 0) {
-            reiniciaPelota();
-            velocidadX *= -1;
-            velocidadX = 8;
-            velocidadY = 6;
-        }
-    }, 30);
+            // COLISIÓN CON LA CPU
+            for (let i = 0; i < colisionPelota.length; i++) {
+                let punto = colisionPelota[i];
+                if (posicionX + radio > cpuX && posicionX - radio < cpuX + cpuAncho && 
+                    posicionY + radio > cpuY && posicionY - radio < cpuY + cpuAltura) {
+                    velocidadX += 2;
+                    velocidadY += 2;
+                    velocidadX *= -1;
+                    break;
+                }
+            }
+
+            // MOVER LA CPU AUTOMÁTICAMENTE
+            if (posicionY > cpuY + cpuAltura / 2 && (cpuY + cpuAltura) < limiteY) {
+                cpuY += 5; 
+            } else if (posicionY < cpuY + cpuAltura / 2 && cpuY > 0) {
+                cpuY -= 5; 
+            }
+            cpu.setAttribute("y", cpuY);
+
+            // COMPROBAR SI ALGÚN JUGADOR OBTUVO UN PUNTO Y REINICIAR LA PELOTA EN CASO AFIRMATIVO
+            const punto = compruebaPunto(posicionX, limiteX, radio);
+            if (punto > 0) {
+                actualizarMarcador();
+                reiniciaPelota();
+                velocidadX *= -1;
+                velocidadX = 8;
+                velocidadY = 6;
+
+                if(punto == 1){
+                    for(let i = 0; i < 10; i++){
+                        cambioColor(jugador, i * 100);
+                    }
+                } else if(punto == 2){
+                    for(let i = 0; i < 10; i++){
+                        cambioColor(cpu, i * 100);
+                    }
+                }
+            }
+        }, 30);
+    }
 
     // FUNCION QUE COMPRUEBA LA COLISIÓN DE LA PELOTA CON LAS PORTERIAS
     function compruebaPunto(posicionX, limiteX, radio) {
         if ((posicionX + radio) >= limiteX) {
-            console.log("Punto para el jugador");
             puntuacionJugador++;
-            actualizarMarcador();
             return 1;
         } else if ((posicionX - radio) <= 10) {
-            console.log("Punto para la CPU");
             puntuacionCPU++;
-            actualizarMarcador();
             return 2;
         }
         return 0;
@@ -132,6 +153,18 @@ window.onload = () => {
     }
 
     function actualizarMarcador() {
-        document.getElementById("marcador").textContent = `${puntuacionJugador} - ${puntuacionCPU}`;
+        document.getElementById("marcadorJug").textContent = `${puntuacionJugador}`;
+        document.getElementById("marcadorCPU").textContent = `${puntuacionCPU}`;
+    }
+
+    function cambioColor(objeto, delay) {
+        setTimeout(() => {
+            let rojo = Math.floor(Math.random() * 256);
+            let verde = Math.floor(Math.random() * 256);
+            let azul = Math.floor(Math.random() * 256);
+            let rgbColor = `rgb(${rojo}, ${verde}, ${azul})`;
+
+            objeto.setAttribute("fill", rgbColor);
+        }, delay); 
     }
 }
